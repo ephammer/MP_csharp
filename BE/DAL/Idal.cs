54743c5841843b8f607b658c3bc6b707e77ca153
ephammer <kidon.613@gmail.com>
2017-01-02 11:31:16 +0200

    Update Idal.cs

--- BE/DAL/Idal.cs
+++ BE/DAL/Idal.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BE;
 
 namespace DAL
 {
     interface Idal
     {
+        void AddSpecialization(Specialization specialisation);
+		void RemoveSpecialization(Specialization specialisation);
+		// TODO: Function to update the Specialization
+
+		void AddEmployer(Employer employer);
+		void RemoveEmployer(Employer employer);
+		// TODO: Function to update the Employer
+
+		void AddEmployee(Employee employee);
+		void RemoverEmployee(Employee employee);
+		// TODO: Function to update the Employee
+
+		void AddContract(Contract contract);
+		void RemoveContract(Contract contract);
+		// TODO: Function to update the Contract
+
+		List<Specialization> ListSpecialzation { get; set;}
+		List<Employee> ListEmployees { get; set;}
+		List<Employer> ListEmployer { get; set;}
+		List<Contract> ListContract { get; set;}
+
+		BankAccount ListBankBranches();
     }
 }
